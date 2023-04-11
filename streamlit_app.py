4049936720937499e785ea0abe59e3422d5ee7be
Luisa de La Vega <95625388+luisadelavega@users.noreply.github.com>
2023-04-11 11:45:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,5 @@ language = ['English','Dutch']
 selected_language = st.selectbox('Select the language', language)
 
 # Create a text box for user input
-daterange_input = st.text_input("Enter the keywords here",daterange)
+daterange_input = st.text_input("Enter the keywords here")
 
-# Create a text box for user input
-datefrequency_input = st.text_input("Enter the keywords here",datefrequency)
-
-# Create a text box for user input
-keywords_input = st.text_input("Enter the keywords here",keywords)
