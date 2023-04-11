af56370998d57ce826a7851df42c3d58df482b18
Luisa de La Vega <95625388+luisadelavega@users.noreply.github.com>
2023-04-11 14:39:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,10 @@ import streamlit as st
 
 options = ['News','Tweets']
 selected_option = st.selectbox('Select a data source', options)
+
+# Define a default date value
+default_date = datetime.date(2023, 4, 11)
+date = st.date_input("Enter a start date", value=default_date, min_value=default_date, max_value=default_date + datetime.timedelta(days=7), format="%Y-%m-%d")
            
 language = ['English','Dutch']
 selected_language = st.multiselect('Select the language', language)
@@ -19,7 +23,7 @@ location = ['USA','UK']
 selected_language = st.multiselect('Select the location', location)
 
 # Create a text box for user input
-daterange_input = st.text_input("Enter the keywords here")
+keyword_input = st.text_input("Enter the keywords here")
 
 output_type = ['csv','excel']
 selected_option = st.selectbox('Select a file format', output_type)
