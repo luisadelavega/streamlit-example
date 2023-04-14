313d33f4944e82c286c4d2696f3ccf5adfc4b414
EgemenIpek <egemenipekicloud@icloud.com>
2023-04-14 14:09:57 +0200

    defining 00_serp as a function

--- 00_SERP-ST-BACK.py
+++ 00_SERP-ST-BACK.py
@@ -6,7 +6,7 @@ from datetime import date, timedelta
 warnings.filterwarnings('ignore')
 
 
-def serp_00(API_key, data_source, date_start, date_end, date_frequency='', language, locations, keywords, domains='', filtering_criteria='',):
+def serp_00(api_key, data_source, date_start, date_end, date_frequency='', language, locations, keywords, domains='', filtering_criteria='',):
     #defining the days
     start = date(date_start)
     end = date(date_end)
@@ -20,7 +20,7 @@ def serp_00(API_key, data_source, date_start, date_end, date_frequency='', langu
     for i in range(0, len(days)-1):
         for location in locations:
             params = {
-                'api_key': API_key
-                'search_type': data_source
+                'api_key': api_key,
+                'search_type': data_source,
                 'q':
             }
